895b5128e72f5820b536a1768ec9724fd4b29c30
Josh Wayles <Joshwayles@gmail.com>
2024-10-18 16:26:05 +0200

    Website 2.0
    Files uploaded to test new site

---
+++ src/script.js
@@ -0,0 +1,13 @@
+//Meowsteroid thumb image change
+const meowsteroids = document.getElementById('meowsteroids-img')
+
+function meowImageChange(inp) {
+    meowsteroids.src = inp.src;
+}
+
+//Gator thumb image change
+const gator = document.getElementById('gator-img')
+
+function gatorImageChange(inp) {
+    gator.src = inp.src;
+}
