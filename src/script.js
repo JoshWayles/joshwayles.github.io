76de09168caaef1b16689b2250b19677d09cfac1
Josh Wayles <Joshwayles@gmail.com>
2024-11-13 10:08:21 +0100

    Project updates as well as small code refactoring

--- src/script.js
+++ src/script.js
@@ -11,3 +11,10 @@ const gator = document.getElementById('gator-img')
 function gatorImageChange(inp) {
     gator.src = inp.src;
 }
+
+//Wolf thumb image change
+const wolf = document.getElementById('wolf-img')
+
+function wolfImageChange(inp) {
+    wolf.src = inp.src;
+}
