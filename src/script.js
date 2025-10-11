e09eedd5db507dae07c96787d88b76ed2eb6ac5f
Josh Wayles <Joshwayles@gmail.com>
2025-10-11 20:06:08 +0200

    Re upload

---
+++ src/script.js
@@ -0,0 +1,11 @@
+//NAV BAR FUNCTIONALITY//
+
+const nav = document.getElementById('nav-menu')
+
+function navOpen() {
+    nav.classList.add('nav-open')
+}
+
+function navClose() {
+    nav.classList.remove('nav-open')
+}
